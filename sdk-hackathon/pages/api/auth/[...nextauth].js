aaf7925f1645791a32aa5ad9f84e12677de0e592
withsang <jsa5115@naver.com>
2022-08-05 23:22:43 +0900

    edit nextauth

--- sdk-hackathon/pages/api/auth/[...nextauth].js
+++ sdk-hackathon/pages/api/auth/[...nextauth].js
@@ -1,6 +1,10 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-export default NextAuth({
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve('..', '..', '..', '.env') });
+
+const options = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -9,4 +13,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-})
+  database: process.env.DATABASE_URL,
+};
+
+export default NextAuth(options);
