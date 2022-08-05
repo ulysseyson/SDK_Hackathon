597b8f5f49dd3cc6e703cf1010124c36d43c15d6
withsang <jsa5115@naver.com>
2022-08-06 01:15:16 +0900

    Add Login button

--- sdk-hackathon/pages/api/auth/[...nextauth].js
+++ sdk-hackathon/pages/api/auth/[...nextauth].js
@@ -16,15 +16,6 @@ const options = {
   database: process.env.DATABASE_URL,
   debug: process.env.NODE_ENV === 'development',
   secret: process.env.AUTH_SECRET,
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = 'admin';
-      return token;
-    },
-  },
 };
 
 export default NextAuth(options);
--- sdk-hackathon/pages/index.js
+++ sdk-hackathon/pages/index.js
@@ -1,18 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Link from 'next/link';
+import LoginBtn from '../components/login-btn';
 
 export default function Home() {
   return (
     <div>
-      <Link href="api/auth/signin">
-        <a>Sign In</a>
-      </Link>
-      <Link href="api/auth/signout">
-        <a>Sign Out</a>
-      </Link>
-      Start!
+      <LoginBtn />
+      <p>Start!</p>
     </div>
   );
 }
