11ef5f07bd38b5b271fb035f075effce031ad6fc
withsang <jsa5115@naver.com>
2022-08-06 08:07:27 +0900

    /api/posts/like

---
+++ sdk-hackathon/prisma/migrations/20220805224020_11th/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `likedBy` on the `UsersOnPosts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UsersOnPosts" DROP COLUMN "likedBy";
