0bf3a23bcbebd861bd86759cd84808ec9c86e4ed
withsang <jsa5115@naver.com>
2022-08-06 04:49:41 +0900

    Try to create session

--- sdk-hackathon/pages/api/auth/[...nextauth].js
+++ sdk-hackathon/pages/api/auth/[...nextauth].js
@@ -1,11 +1,17 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaAdapter } from '../../../prisma/adapter';
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({ path: path.resolve('..', '..', '..', '.env') });
 
+const prisma = new PrismaClient();
+
 const options = {
   // Configure one or more authentication providers
+  adapter: new PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
